f17e14c2365920d2ae0f268e387823f06f0735c2
Ivan Jasjko <i.jasjko@elsevier.com>
2019-08-21 14:48:09 +0100

    Renamed some vals

--- src/main/scala/holdemij/Dealer.scala
+++ src/main/scala/holdemij/Dealer.scala
@@ -2,8 +2,8 @@ package holdemij
 
 class Dealer(cards: Seq[Card]) {
 
-  val botHand: Seq[Card] = cards.slice(0,2)
-  val playerHand: Seq[Card] = cards.slice(2,4)
+  val firstHand: Seq[Card] = cards.slice(0,2)
+  val secondHand: Seq[Card] = cards.slice(2,4)
   val flop: Seq[Card] = cards.slice(5,8)
   val turn: Seq[Card] = cards.slice(9,10)
   val river: Seq[Card] = cards.slice(11,12)
--- src/main/scala/holdemij/GamePrinter.scala
+++ src/main/scala/holdemij/GamePrinter.scala
@@ -2,8 +2,8 @@ package holdemij
 
 object GamePrinter {
   def printGame(dealer: Dealer) = {
-    println(s"1st Hand: ${dealer.playerHand}")
-    println(s"2nd Hand: ${dealer.botHand}")
+    println(s"1st Hand: ${dealer.firstHand}")
+    println(s"2nd Hand: ${dealer.secondHand}")
     println(s"FLOP: ${dealer.flop}")
     println(s"TURN: ${dealer.turn}")
     println(s"RIVER: ${dealer.river}")
